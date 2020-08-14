594b463c498d29b39aea3f623cca6e8e9a4317c3
Shinchirou <b.ostaszewski@gmail.com>
2020-08-14 21:22:54 +0200

    cities

--- src/main/java/com/app/controller/CityController.java
+++ src/main/java/com/app/controller/CityController.java
@@ -1,11 +1,16 @@
 package com.app.controller;
 
+import com.app.model.Cinema;
 import com.app.model.City;
+import com.app.model.thymeleaf.CinemaWithObj;
+import com.app.model.thymeleaf.CityWithObj;
 import com.app.service.CityService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Controller
@@ -16,18 +21,28 @@ public class CityController {
     private final CityService cityService;
 
     @GetMapping("/delete/{id}")
-    public Integer deleteCity (@PathVariable Integer id){
+    public Integer deleteCity(@PathVariable Integer id) {
         return cityService.deleteCity(id);
     }
 
     @GetMapping("/{id}")
-    public City showById (@PathVariable Integer id){
+    public City showById(@PathVariable Integer id) {
         return cityService.findCityById(id);
     }
 
     @GetMapping("/all")
-    public List<City> getAll(){
-        return cityService.getAll();
+    public String getAll(Model model) {
+        List<CityWithObj> cities = new ArrayList<>();
+        for (City c : cityService.getAll()) {
+            cities.add(CityWithObj.builder()
+                    .id(c.getId())
+                    .name(c.getName())
+                    .build());
+
+
+        }
+        model.addAttribute("cities", cities);
+        return "cities";
     }
 
 }
---
+++ src/main/java/com/app/model/thymeleaf/CityWithObj.java
@@ -0,0 +1,21 @@
+package com.app.model.thymeleaf;
+
+import com.app.model.Cinema;
+import com.app.model.CinemaRoom;
+import com.app.model.Movie;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Data
+public class CityWithObj {
+    private Integer id;
+    private String name;
+}
